aee672e2d4d14d8ac524cfd67dc4fff5b18a4db2
Jakob Runge <sicarius@g4t3.de>
2015-10-21 15:26:55 +0200

    Set nginx.conf: sendfile off;
    - There is currently a bug with VirtualBox,
      where sendfile returns cached content.
      This leads to strange errors in some cases.
      As a workaround sendfile will now be turned off in nginx.

--- install.sh
+++ install.sh
@@ -31,6 +31,12 @@ systemctl stop nginx
 rm /etc/nginx/sites-enabled/default
 cp /containerSetup/nginx/soundcomparisons /etc/nginx/sites-available
 ln -s /etc/nginx/sites-available/soundcomparisons /etc/nginx/sites-enabled/soundcomparisons
+# Setting sendfile off:
+# https://docs.vagrantup.com/v2/synced-folders/virtualbox.html
+# https://github.com/mitchellh/vagrant/issues/351#issuecomment-1339640
+# https://stackoverflow.com/a/23487194/448591
+sed -i.bak "s/sendfile on/sendfile off/g" /etc/nginx/nginx.conf
+rm /etc/nginx/nginx.conf.bak
 systemctl start nginx
 # Fetching copy of website repository:
 echo "Taking care of website submodule:"
