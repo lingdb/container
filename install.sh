09a5a2790f386f28d594328a54c1c5f195305c6f
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-11-03 12:59:57 +0100

    Work to integrate lexdb
    - Remember to fix install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+echo "Updating package database:"
+apt-get update
 # Installing mariadb:
-/containerSetup/install/mariadb.sh
+#/containerSetup/install/mariadb.sh
 # Setup for nginx:
-/containerSetup/install/nginx.sh
+#/containerSetup/install/nginx.sh
 # Installing soundcomparisons service:
-/containerSetup/install/soundcomparisons.sh
+#/containerSetup/install/soundcomparisons.sh
+# Installing postgresql:
+/containerSetup/install/postgresql.sh
 # Installing lexdb service:
 /containerSetup/install/lexdb.sh
--- install/lexdb.sh
+++ install/lexdb.sh
@@ -3,8 +3,19 @@
 # Got to move .gitignore out of the way intermediate,
 # because hg won't clone into a non empty repo.
 echo "Taking care of lexdb:"
-apt-get install -y mercurial
+apt-get install -y mercurial python-virtualenv python-pip build-essential python-dev postgresql-server-dev-9.3 libxml2-dev libxslt-dev
 mv /containerSetup/lexdb/.gitignore /tmp/lexdb.gitignore
-hg clone https://bitbucket.org/evoling/lexdb /containerSetup/lexdb
+sudo -u vagrant hg clone https://bitbucket.org/evoling/lexdb /containerSetup/lexdb
 mv /tmp/lexdb.gitignore /containerSetup/lexdb/.gitignore
-hg checkout -R /containerSetup/lexdb py2.7-django1.8
+sudo -u vagrant hg checkout -R /containerSetup/lexdb py2.7-django1.8
+# Setting up environment and installing packages:
+sudo -u vagrant virtualenv -p python2.7 /containerSetup/lexdb
+#cp /containerSetup/requirements.txt /containerSetup/lexdb
+chdir /containerSetup/lexdb
+sudo -u vagrant ./bin/pip -r REQUIREMENTS
+sudo -u vagrant ./bin/pip -r REQUIREMENTS-DEV
+# SyncDB:
+# FIXME how to make step below non interactive?!
+./bin/python manage.py syncdb
+# Adjust ALLOWED_HOSTS:
+sed -i.bak "s/ALLOWED_HOSTS = [/ALLOWED_HOSTS = [\"127.0.0.1:5001\"g" ./ielex/local_settings.py
