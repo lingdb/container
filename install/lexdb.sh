b13f760e44bd1c2fadd82a84788de7f4a663da9a
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-11-04 12:14:41 +0100

    Work around in install/lexdb.sh
    - Compare #2

--- install/lexdb.sh
+++ install/lexdb.sh
@@ -3,19 +3,19 @@
 # Got to move .gitignore out of the way intermediate,
 # because hg won't clone into a non empty repo.
 echo "Taking care of lexdb:"
-apt-get install -y mercurial python-virtualenv python-pip build-essential python-dev postgresql-server-dev-9.3 libxml2-dev libxslt-dev
+apt-get install -y mercurial python-virtualenv python-pip build-essential python-dev postgresql-server-dev-9.3 libxml2-dev libxslt-dev python2.7-lxml
 mv /containerSetup/lexdb/.gitignore /tmp/lexdb.gitignore
 sudo -u vagrant hg clone https://bitbucket.org/evoling/lexdb /containerSetup/lexdb
 mv /tmp/lexdb.gitignore /containerSetup/lexdb/.gitignore
 sudo -u vagrant hg checkout -R /containerSetup/lexdb py2.7-django1.8
 # Setting up environment and installing packages:
 sudo -u vagrant virtualenv -p python2.7 /containerSetup/lexdb
-#cp /containerSetup/requirements.txt /containerSetup/lexdb
-chdir /containerSetup/lexdb
-sudo -u vagrant ./bin/pip -r REQUIREMENTS
-sudo -u vagrant ./bin/pip -r REQUIREMENTS-DEV
+sudo -u vagrant /containerSetup/lexdb/bin/pip install -r /containerSetup/lexdb/REQUIREMENTS
+# Workaround because of build problems with lxml, see https://github.com/lingdb/container/issues/2:
+grep -v '#' /containerSetup/lexdb/REQUIREMENTS-DEV | grep -v lxml | xargs sudp -u vagrant /containerSetup/lexdb/bin/pip install
+#sudo -u vagrant /containerSetup/lexdb/bin/pip install -r /containerSetup/lexdb/REQUIREMENTS-DEV
 # SyncDB:
 # FIXME how to make step below non interactive?!
-./bin/python manage.py syncdb
+#./bin/python manage.py syncdb
 # Adjust ALLOWED_HOSTS:
-sed -i.bak "s/ALLOWED_HOSTS = [/ALLOWED_HOSTS = [\"127.0.0.1:5001\"g" ./ielex/local_settings.py
+#sed -i.bak "s/ALLOWED_HOSTS = [/ALLOWED_HOSTS = [\"127.0.0.1:5001\"g" ./ielex/local_settings.py
