bccfc9e142ec6e62fadd4662867a25639fe48778
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-11-04 13:44:40 +0100

    Experiment with nginx settings
    - Changed Vagrant and nginx ports.
      Need to correct this to 80 later.

--- install/nginx.sh
+++ install/nginx.sh
@@ -4,9 +4,11 @@ echo "Installing nginx:"
 apt-get install -y nginx
 service nginx stop
 # Config for nginx & start:
-rm /etc/nginx/sites-enabled/default
-ln -s /containerSetup/nginx/soundcomparisons /etc/nginx/sites-available/soundcomparisons
-ln -s /etc/nginx/sites-available/soundcomparisons /etc/nginx/sites-enabled/soundcomparisons
+enabled="/etc/nginx/sites-enabled"
+available="/etc/nginx/sites-available"
+rm $enabled/default
+ln -s /containerSetup/nginx/siteconfig $available/siteconfig
+ln -s $available/siteconfig $enabled/siteconfig
 # Setting sendfile off:
 # https://docs.vagrantup.com/v2/synced-folders/virtualbox.html
 # https://github.com/mitchellh/vagrant/issues/351#issuecomment-1339640
