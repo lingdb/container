55f9b2d1c635a67812108dcae7a5638a53d48037
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-11-04 13:19:51 +0100

    Improve install/lexdb.sh

--- install/lexdb.sh
+++ install/lexdb.sh
@@ -3,19 +3,30 @@
 # Got to move .gitignore out of the way intermediate,
 # because hg won't clone into a non empty repo.
 echo "Taking care of lexdb:"
-apt-get install -y mercurial python-virtualenv python-pip build-essential python-dev postgresql-server-dev-9.3 libxml2-dev libxslt-dev python2.7-lxml
-mv /containerSetup/lexdb/.gitignore /tmp/lexdb.gitignore
-sudo -u vagrant hg clone https://bitbucket.org/evoling/lexdb /containerSetup/lexdb
-mv /tmp/lexdb.gitignore /containerSetup/lexdb/.gitignore
-sudo -u vagrant hg checkout -R /containerSetup/lexdb py2.7-django1.8
+apt-get install -y mercurial python-virtualenv python-pip build-essential python-dev postgresql-server-dev-9.3 libxml2-dev libxslt-dev python2.7-lxml pwgen
+lexdb="/containerSetup/lexdb"
+mv $lexdb/.gitignore /tmp/lexdb.gitignore
+sudo -u vagrant hg clone https://bitbucket.org/evoling/lexdb $lexdb
+mv /tmp/lexdb.gitignore $lexdb/.gitignore
+sudo -u vagrant hg checkout -R $lexdb py2.7-django1.8
 # Setting up environment and installing packages:
-sudo -u vagrant virtualenv -p python2.7 /containerSetup/lexdb
-sudo -u vagrant /containerSetup/lexdb/bin/pip install -r /containerSetup/lexdb/REQUIREMENTS
+sudo -u vagrant virtualenv -p python2.7 $lexdb
+sudo -u vagrant $lexdb/bin/pip install -r $lexdb/REQUIREMENTS
 # Workaround because of build problems with lxml, see https://github.com/lingdb/container/issues/2:
-grep -v '#' /containerSetup/lexdb/REQUIREMENTS-DEV | grep -v lxml | xargs sudp -u vagrant /containerSetup/lexdb/bin/pip install
-#sudo -u vagrant /containerSetup/lexdb/bin/pip install -r /containerSetup/lexdb/REQUIREMENTS-DEV
+grep -v '#' $lexdb/REQUIREMENTS-DEV | grep -v lxml | xargs sudp -u vagrant $lexdb/bin/pip install
+echo "Editing …ielex/local_settings.py"
+file="$lexdb/ielex/local_settings.py"
+cp $file.dist $file
+sed -i.bak "s/'ENGINE': 'django.db.backends.sqlite3',/'ENGINE':'django.db.backends.postgresql_psycopg2',/" $file
+sed -i.bak "s/'NAME': os.path.join(ROOTDIR, 'db.sqlite3'),/'NAME':'ielexdb201510',/" $file
+sed -i.bak "s/'USER': '',.*$/'USER':'ielexuser',/" $file
+sed -i.bak "s/'PASSWORD': '',.*$/'PASSWORD':'abcd1234',/" $file
+sed -i.bak "s/'HOST': '',.*$/'HOST':'localhost',/" $file
+sed -i.bak "s/'^.*PORT': '',.*$//" $file
+sed -i.bak "s/ALLOWED_HOSTS = \[/ALLOWED_HOSTS = \[\"127.0.0.1:5001\"/" $file
+secret=$(pwgen 32 1)
+sed -i.bak "s/SECRET_KEY = \"<++>\"/SECRET_KEY = \"$secret\"/" $file
+rm $file.bak
 # SyncDB:
 # FIXME how to make step below non interactive?!
 #./bin/python manage.py syncdb
-# Adjust ALLOWED_HOSTS:
-#sed -i.bak "s/ALLOWED_HOSTS = [/ALLOWED_HOSTS = [\"127.0.0.1:5001\"g" ./ielex/local_settings.py
