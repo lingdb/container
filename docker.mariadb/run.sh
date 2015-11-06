7e2fe9adab57e9db79f9cf169409950c9b829a90
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-11-06 22:31:42 +0100

    Format docker.mariadb/run.sh

--- docker.mariadb/run.sh
+++ docker.mariadb/run.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/bash
 # https://hub.docker.com/_/mariadb/
-docker run --name container.mariadb -e MYSQL_ROOT_PASSWORD=1234 -e MYSQL_DATABASE=v5 -d mariadb:latest
+docker run --name container.mariadb \
+           -e MYSQL_ROOT_PASSWORD=1234 \
+           -e MYSQL_DATABASE=v5 \
+           -d mariadb:latest
