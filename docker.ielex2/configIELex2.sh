447e62ad5b8be5887e990984b03e5f86e8a393e3
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-11-07 14:13:37 +0100

    Had docker.ielex2 running the first time
    - Need to:
      - Automate setup procedure
      - Integrate with docker.nginx
      - Specify entrypoint

--- docker.ielex2/configIELex2.sh
+++ docker.ielex2/configIELex2.sh
@@ -8,8 +8,8 @@ sed -i.bak "s/'ENGINE': 'django.db.backends.sqlite3',/'ENGINE':'django.db.backen
 sed -i.bak "s/'NAME': os.path.join(ROOTDIR, 'db.sqlite3'),/'NAME':'ielexdb201510',/" $file
 sed -i.bak "s/'USER': '',.*$/'USER':'ielexuser',/" $file
 sed -i.bak "s/'PASSWORD': '',.*$/'PASSWORD':'abcd1234',/" $file
-sed -i.bak "s/'HOST': '',.*$/'HOST':'localhost',/" $file
-sed -i.bak "s/'PORT': '',.*$/'PORT': '5000',/" $file
+sed -i.bak "s/'HOST': '',.*$/'HOST':'postgres',/" $file
+sed -i.bak "s/'PORT': '',.*$/'PORT': '5432',/" $file
 sed -i.bak "s/ALLOWED_HOSTS = \[/ALLOWED_HOSTS = \[\"127.0.0.1\"/" $file
 sed -i.bak "s/STATIC_ROOT = .\"/STATIC_ROOT = \"ielex\/static\/\"/" $file
 secret=$(pwgen 32 1)
--- docker.ielex2/run.sh
+++ docker.ielex2/run.sh
@@ -3,7 +3,7 @@
 last=$(docker ps -f "name=lingdb.postgres" -f status=running -n=1 --format "{{.Names}}")
 image="lingdb.ielex2"
 echo "Running $image against $last…"
-#docker run --link $last:mysql \
-#           -p 127.0.0.1:5000:5000 \
-#           -v `pwd`/sound:/sndcomp/static/sound \
-#           -d $image
+docker run --link $last:postgres \
+           -p 127.0.0.1:5001:5000 \
+           -v `pwd`/sound:/sndcomp/static/sound \
+           -it $image bash
