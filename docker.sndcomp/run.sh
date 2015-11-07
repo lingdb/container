136717acbb7f5e2aec3976ba8e7dad4dd78e2305
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-11-07 01:13:37 +0100

    Include soundfiles in docker.sndcomp
    - Add sound/.gitignore, .dockerignore

--- docker.sndcomp/run.sh
+++ docker.sndcomp/run.sh
@@ -2,4 +2,5 @@
 # http://www.postgresql.org/docs/9.0/static/libpq-envars.html
 docker run --link lingdb.mariadb:mysql \
            -p 127.0.0.1:5000:5000 \
+           -v `pwd`/sound:/sndcomp/static/sound \
            -it lingdb.sndcomp
