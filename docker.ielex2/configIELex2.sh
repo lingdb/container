affb20d9ad606b22b6d5411335877f4f717820ad
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-11-07 13:31:02 +0100

    Improve docker.ielex2/{Dockerfile,configIELex2.sh}

--- docker.ielex2/configIELex2.sh
+++ docker.ielex2/configIELex2.sh
@@ -9,9 +9,9 @@ sed -i.bak "s/'NAME': os.path.join(ROOTDIR, 'db.sqlite3'),/'NAME':'ielexdb201510
 sed -i.bak "s/'USER': '',.*$/'USER':'ielexuser',/" $file
 sed -i.bak "s/'PASSWORD': '',.*$/'PASSWORD':'abcd1234',/" $file
 sed -i.bak "s/'HOST': '',.*$/'HOST':'localhost',/" $file
-sed -i.bak "s/'^.*PORT': '',.*$//" $file
+sed -i.bak "s/'PORT': '',.*$/'PORT': '5000',/" $file
 sed -i.bak "s/ALLOWED_HOSTS = \[/ALLOWED_HOSTS = \[\"127.0.0.1\"/" $file
-sed -i.bak "s/STATIC_ROOT = \"\"/STATIC_ROOT = \"ielex/static/\"/" $file
+sed -i.bak "s/STATIC_ROOT = .\"/STATIC_ROOT = \"ielex\/static\/\"/" $file
 secret=$(pwgen 32 1)
 sed -i.bak "s/SECRET_KEY = \"<++>\"/SECRET_KEY = \"$secret\"/" $file
 rm $file.bak
