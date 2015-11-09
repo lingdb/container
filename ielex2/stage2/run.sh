41984873693c0e533bdffd0a9acfa9bd8e7a8d7e
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-11-09 14:43:08 +0100

    Add test option to ielex2/stage2/run.sh
    - Executing `run.sh test` now forwards ielex2 container port 127.0.0.1:8080:5000

--- ielex2/stage2/run.sh
+++ ielex2/stage2/run.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 # http://www.postgresql.org/docs/9.0/static/libpq-envars.html
+# When passing test as first parameter, binds to 127.0.0.1:8080.
 last=$(docker ps -f "name=lingdb.postgres" -f status=running -n=1 -q)
 image="lingdb.ielex2.stage2"
 name="$image.$(date -I).$(pwgen 5 1)"
 echo "Running $image against $last…"
-docker run --link $last:postgres \
-           --name $name \
-           -d $image
+case $1 in
+  test)
+    docker run --link $last:postgres \
+               --name $name \
+               -p 127.0.0.1:8080:5000 \
+               -d $image
+  ;;
+  *)
+    docker run --link $last:postgres \
+               --name $name \
+               -d $image
+  ;;
+esac
