0b30268b94ba4f2c4f8bbe3669921c388c2ab342
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-11-15 00:17:35 +0100

    Add labels to Dockerfiles
    - Added labels are:
      - lingdb:
        This label contains the name of the container in the lingdb/container wiki as well as in the directory structure.
      - version:
        This is the semantic versioning number for the container,
        and I'll see how it will be used in the future.
      - documentation:
        This is the https address of the wiki page describing the container that relates to the Dockerfile.

--- pullBaseImages.sh
+++ pullBaseImages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Pulling all the base images that the containers build upon.
-images="continuumio/anaconda:latest mariadb:latest nginx:latest postgres:9.3.10 ubuntu:latest"
+images="continuumio/anaconda:latest ubuntu:latest mariadb:latest nginx:latest postgres:9.3.10 crosbymichael/skydns:latest crosbymichael/skydock:latest"
 echo "Pulling base images:"
 for image in $images; do
   echo "Pulling image: $image"
