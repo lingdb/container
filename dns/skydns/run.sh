f30783522556c75fced82c89f6d6bd36fc052aa1
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-11-15 15:00:46 +0100

    Make docker0 dns generic
    - Depending on the host version of docker we may have different ip ranges for the host network.
      This messes with the dns setup before this commit as that did not figure out the ip.
      With https://stackoverflow.com/a/14625642/448591 I was able to get the docker0 host ip dynamically.

--- dns/skydns/run.sh
+++ dns/skydns/run.sh
@@ -3,5 +3,6 @@ domain="docker0"
 name="skydns.$domain"
 image="crosbymichael/skydns"
 fallback="8.8.8.8:53,8.8.4.4:53"
-network="172.17.0.1:53:53/udp"
+dockerHostIp=$(ip -f inet -o addr show docker0|cut -d\  -f 7 | cut -d/ -f 1)
+network="$dockerHostIp:53:53/udp"
 docker run -d -p $network --name $name $image -nameserver $fallback -domain $domain
--- nginx/run.sh
+++ nginx/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 image="lingdb/nginx"
 name="lingdb_nginx_$(date -I)_$(pwgen 5 1)"
-dns="172.17.0.1"
+dns=$(ip -f inet -o addr show docker0|cut -d\  -f 7 | cut -d/ -f 1)
 docker run -p 0.0.0.0:80:80 \
            --name $name \
            --dns="$dns" \
