4ca850ea807d60dfe75796349fa86492d9025c73
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-11-15 14:41:57 +0100

    Rename container images for ielex2
    - Reason here is the same as for b958371a51c1affe94044a843da7c6aa89922c66.
    - Nginx will need adjustment to this, too.
    - Renamed:
      lingdb/ielex2/stage0 -> lingdb/ielex2_stage0
      lingdb/ielex2/stage1 -> lingdb/ielex2_stage1
      lingdb/ielex2/stage2 -> lingdb/ielex2_stage2

--- ielex2/stage1/run.sh
+++ ielex2/stage1/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # http://www.postgresql.org/docs/9.0/static/libpq-envars.html
 last=$(docker ps -f "name=lingdb_postgres" -f status=running -n=1 -q)
-image="lingdb/ielex2/stage0"
+image="lingdb/ielex2_stage0"
 name="lingdb_ielex2_stage0_$(date -I)_$(pwgen 5 1)"
 echo "Running $image against $last…"
 docker run --link $last:postgres \
            --name $name \
            -it $image bash
 echo "Committing $name as stage1…"
-docker commit -a "Jakob Runge <mam09crm@studserv.uni-leipzig.de>" $name lingdb/ielex2/stage1
+docker commit -a "Jakob Runge <mam09crm@studserv.uni-leipzig.de>" $name lingdb/ielex2_stage1
--- ielex2/stage2/build.sh
+++ ielex2/stage2/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t lingdb/ielex2/stage2 .
+docker build -t lingdb/ielex2_stage2 .
