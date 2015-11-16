23049cca927dc755c5f89a524952dda581c1318e
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-11-16 22:12:13 +0100

    Adjust ielex2/stage1/build.sh to current ielex2 repo

--- ielex2/stage1/build.sh
+++ ielex2/stage1/build.sh
@@ -4,7 +4,7 @@ echo "Editing …ielex/local_settings.py"
 file="$dir/ielex/local_settings.py"
 cp $file.dist $file
 sed -i.bak "s/'ENGINE': 'django.db.backends.sqlite3',/'ENGINE':'django.db.backends.postgresql_psycopg2',/" $file
-sed -i.bak "s/'NAME': os.path.join(ROOTDIR, 'db.sqlite3'),/'NAME':'ielexdb201510',/" $file
+sed -i.bak "s/'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),/'NAME':'ielexdb201510',/" $file
 sed -i.bak "s/'USER': '',.*$/'USER':'ielexuser',/" $file
 sed -i.bak "s/'PASSWORD': '',.*$/'PASSWORD':'abcd1234',/" $file
 sed -i.bak "s/'HOST': '',.*$/'HOST':'postgres',/" $file
