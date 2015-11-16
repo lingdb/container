1f90dec767852c287917590a0e45067c283f165f
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-11-16 13:09:52 +0100

    Make dns resolver in nginx/siteconfig generic
    - The way to do this is that build.sh creates a siteconfig.tmp
      that contains the host specific resolver, and that this file is copied into the container.
      After build siteconfig.tmp is deleted.
    - Close #9

--- nginx/build.sh
+++ nginx/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+dns=$(ip -f inet -o addr show docker0|cut -d\  -f 7 | cut -d/ -f 1)
+sed "s/<+RESOLVER+>/$dns/g" siteconfig > siteconfig.tmp
 image="lingdb/nginx"
 tag=$(date -I | sed -e 's/-/./g')
 docker build -t $image:$tag .
 docker tag -f $image:$tag $image:latest
+rm siteconfig.tmp
