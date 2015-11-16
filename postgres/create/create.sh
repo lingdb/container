8e47206d65b35345c3c0b4963ca7991e20e92d0b
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-11-16 16:25:45 +0100

    Bugfix in postgres/create/create.sh
    - The problem was not the backup script,
      but that the create script didn't insert into the correct database.
    - Close #10.

--- postgres/create/create.sh
+++ postgres/create/create.sh
@@ -6,7 +6,7 @@ echo "DROP DATABASE IF EXISTS ielexdb201510;" | $pg
 echo "CREATE DATABASE ielexdb201510 WITH OWNER = ielexuser;" | $pg
 if [ -f dump.sql ]; then
   echo "Found dump.sql -> inserting…"
-  $pg < dump.sql
+  $pg ielexdb201510 < dump.sql
 else
   echo "No dump.sql found -> not inserting one"
 fi
