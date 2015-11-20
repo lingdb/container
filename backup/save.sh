f3ddfbe65670ca56b52ad313bf5d9dd47b6eec7b
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-11-20 12:19:22 +0100

    Adjust backup/save.sh
    - Now only saving images with label=lingdb.
      This makes backups more precise to the project and may also reduce backup size.

--- backup/save.sh
+++ backup/save.sh
@@ -5,7 +5,7 @@ cd $dir
 # Creating new backup:
 name="$(date -I).images.tar"
 echo "Creating $name…"
-docker images -q | xargs docker save -o $name
+docker images --filter "label=lingdb" -q | xargs docker save -o $name
 # Removing some to keep last 5:
 echo "Keeping only 5 latest backups…"
 ls -tr *tar | head -n -5 | xargs --no-run-if-empty rm
