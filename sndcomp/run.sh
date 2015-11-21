c7011d0df759d0e4c74aa6beb284d0942c28e562
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-11-21 11:15:03 +0100

    Update build.sh

--- sndcomp/run.sh
+++ sndcomp/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-# http://www.postgresql.org/docs/9.0/static/libpq-envars.html
+# Switch into container/nginx:
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $dir
+cd ../nginx
+# Start sndcomp container from container/nginx:
 last=$(docker ps -f "name=lingdb_mariadb" -f status=running -n=1 -q)
 image="lingdb/sndcomp"
 name="lingdb_sndcomp_$(date -I)_$(pwgen 5 1)"
@@ -9,5 +13,6 @@ else
   echo "Running $image against $last…"
   docker run --link $last:mysql \
              --name $name \
+             -v sound:/sndcomp/static/sound \
              -d $image
 fi
