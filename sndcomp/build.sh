514b6cf0cd66ff344527f6489c803cd15147c1db
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-11-24 16:32:25 +0100

    Adjust sndcomp to #6

--- sndcomp/build.sh
+++ sndcomp/build.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 if [ -f client_secrets.json ]; then
+  dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+  cd $dir
+  source ../secrets/secrets.sh
+  sed -e "s/<SECRET>/$secretMariaDB/g" getSndcomp.sh > getSndcomp.sh.tmp
+  # Building image:
   image="lingdb/sndcomp"
   tag=$(date -I | sed -e 's/-/./g')
   docker build -t $image:$tag .
   docker tag -f $image:$tag $image:latest
+  # Cleanup:
+  rm getSndcomp.sh.tmp
 else
   echo "Won't build lingdb.sndcomp due to missing file: 'client_secrets.json'."
 fi
--- sndcomp/getSndcomp.sh
+++ sndcomp/getSndcomp.sh
@@ -3,7 +3,8 @@ dir="/sndcomp"
 cd $dir
 pip install -r $dir/requirements.txt
 cp $dir/config_example.py $dir/config.py
-sed -i.bak "s/dbURI = 'mysql:\/\/root:1234@localhost\/v5'/dbURI = 'mysql:\/\/root:1234@mysql\/v5'/" $dir/config.py
+secret="<SECRET>"
+sed -i.bak "s/dbURI = 'mysql:\/\/root:1234@localhost\/v5'/dbURI = 'mysql:\/\/root:$secret@mysql\/v5'/" $dir/config.py
 sed -i.bak "s/host='127.0.0.1'/host='0.0.0.0'/" $dir/config.py
 secret=$(pwgen 32 1)
 sed -i.bak "s/Zooj8eegie4sheequ2ohfoh6pu0goKae/$secret/" $dir/config.py
