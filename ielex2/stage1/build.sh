ea3f72420bc92d1ddff909928e59ebc79871efc4
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-11-24 16:44:28 +0100

    Adjust ielex2 to #6

--- ielex2/stage1/build.sh
+++ ielex2/stage1/build.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-dir="$(pwd)/ielex2"
+# Loading secrets:
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $dir
+source ../../secrets/secrets.sh
+# Modifying config:
+dir="./ielex2"
 echo "Editing …ielex/local_settings.py"
 file="$dir/ielex/local_settings.py"
 cp $file.dist $file
 sed -i.bak "s/'ENGINE': 'django.db.backends.sqlite3',/'ENGINE':'django.db.backends.postgresql_psycopg2',/" $file
 sed -i.bak "s/'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),/'NAME':'ielexdb201510',/" $file
 sed -i.bak "s/'USER': '',.*$/'USER':'ielexuser',/" $file
-sed -i.bak "s/'PASSWORD': '',.*$/'PASSWORD':'abcd1234',/" $file
+sed -i.bak "s/'PASSWORD': '',.*$/'PASSWORD':'$secretPostgreSQL',/" $file
 sed -i.bak "s/'HOST': '',.*$/'HOST':'postgres',/" $file
 sed -i.bak "s/'PORT': '',.*$/'PORT': '5432',/" $file
 sed -i.bak "s/ALLOWED_HOSTS = \[/ALLOWED_HOSTS = \[\"127.0.0.1\"/" $file
@@ -14,6 +19,7 @@ sed -i.bak "s/STATIC_ROOT = .\"/STATIC_ROOT = \"ielex\/static\/\"/" $file
 secret=$(pwgen 32 1)
 sed -i.bak "s/SECRET_KEY = \"<++>\"/SECRET_KEY = \"$secret\"/" $file
 rm $file.bak
+# Building image:
 echo "Starting build…"
 image="lingdb/ielex2_stage0"
 tag=$(date -I | sed -e 's/-/./g')
