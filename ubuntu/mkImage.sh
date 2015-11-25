97b619449560cb3253bc7379bdb17daada34f8a7
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-11-25 15:06:22 +0100

    Add extraction of content to ubuntu/mkImage.sh
    - Relates to #16

--- ubuntu/mkImage.sh
+++ ubuntu/mkImage.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
+# https://github.com/lingdb/soundcomparisons/issues/249
+# https://help.ubuntu.com/community/LiveCDCustomization
 # Parameters are:
 # $1: Relative path to the image to use.
-# $2: The device to use for the image.
 # Initial setup, image must be given as $1
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $dir
 image="$dir/$1"
-device="$2"
 mount="$dir/mount"
-# Copy image to destination:
-echo "dd if=$image of=$device"
-sudo dd if=$image of=$device
+extract="$dir/extract-cd"
+# Creating directories in case they don't exist:
+mkdir $mount $extract
 # Mounting device:
-echo "mount $device $mount"
-sudo mount $device $mount
+echo "mount -o loop $image $mount"
+sudo mount -o loop $image $mount
+# Extracting data:
+echo "rsync --exclude=/casper/filesystem.squashfs -a $mount/ $extract"
+sudo rsync --exclude=/casper/filesystem.squashfs -a $mount/ $extract
+echo "unsquashfs $mount/casper/filesystem.squashfs"
+sudo unsquashfs $mount/casper/filesystem.squashfs
+sudo mv squashfs-root edit
 # FIXME DEBUG
 #ls $mount
 #sleep 5s
