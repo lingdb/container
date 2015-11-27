dfc6fcef9d0318beecd17a8eae79e01e44271fc0
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-11-27 15:22:34 +0100

    Improve postgres/create/alter.sql
    - This adjustment to the setup makes it possible to create new users.

--- postgres/create/alter.sql
+++ postgres/create/alter.sql
@@ -1,5 +1,7 @@
 -- Necessary change to django_content_type since Django 1.8:
 ALTER TABLE django_content_type DROP COLUMN name;
+-- Dropping last_login NOT NULL to make sure users can be created:
+ALTER TABLE auth_user ALTER COLUMN last_login DROP NOT NULL;
 -- Remove NOT NULL constraints from tables:
 ALTER TABLE lexicon_cognateclass ALTER COLUMN data DROP NOT NULL;
 ALTER TABLE lexicon_cognatejudgement ALTER COLUMN data DROP NOT NULL;
