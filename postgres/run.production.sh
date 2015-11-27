c55be2b1231ec4050b9cffb688f170f437f880a7
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-11-27 15:42:58 +0100

    Start on #17

---
+++ postgres/run.production.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# https://hub.docker.com/_/postgres/
+# Setup:
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $dir
+source ../secrets/secrets.sh
+last=$(docker ps -f label=lingdb=postgres -f status=exited -n=1 -q)
+name="--name lingdb_postgres_$(date -I)_$(pwgen 5 1)"
+pgsql="-e POSTGRES_USER=ielexuser -e POSTGRES_PASSWORD=$secretPostgreSQL"
+label="-l lingdb=postgres"
+# Action:
+echo "Container to start: $name"
+if [ -z "$last" ]; then
+  echo "No prior container found -> no volumes to reuse…"
+  docker run $name $label $pgsql -d postgres:9.3.10
+else
+  echo "Reusing volumes from $last…"
+  docker run $name $label $pgsql --volumes-from=$last -d postgres:9.3.10
+fi
