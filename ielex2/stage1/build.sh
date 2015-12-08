2a099714f66b93c4e9d85a5408588269c1b4c0e9
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-12-08 11:48:08 +0100

    Update ielex2/stage1/build.sh
    - Add default IELex2 headline and description.

--- ielex2/stage1/build.sh
+++ ielex2/stage1/build.sh
@@ -18,6 +18,9 @@ sed -i.bak "s/ALLOWED_HOSTS = \[/ALLOWED_HOSTS = \[\"127.0.0.1\"/" $file
 sed -i.bak "s/STATIC_ROOT = .\"/STATIC_ROOT = \"ielex\/static\/\"/" $file
 secret=$(pwgen 32 1)
 sed -i.bak "s/SECRET_KEY = \"<++>\"/SECRET_KEY = \"$secret\"/" $file
+sed -i.bak "s/project_long_name = u\"Test Lexical Cognacy Database\"/project_long_name = u\"IELex 2\"/" $file
+sed -i.bak "s/project_short_name = u\"TestLex\"/project_short_name = u\"IELex 2\"/" $file
+sed -i.bak "s/project_description = u\"\"\"Description for the front page\"\"\"/project_description = u\"\"\"Welcome to the IELex 2 website.\"\"\"/" $file
 rm $file.bak
 # Building image:
 echo "Starting build…"
