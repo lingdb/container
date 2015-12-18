3c4f9b051ddef4dd43afabce5ac2241e516831d8
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2015-12-18 19:26:50 +0100

    Adjust backup/save.sh
    - Now keeping only 2 instead of 5 backups around.
      These backups are saved for longterm storage at a different location,
      and are taking up quite some space, so it makes sense to reduce them at this point.

--- backup/save.sh
+++ backup/save.sh
@@ -6,6 +6,6 @@ cd $dir
 name="$(date -I).images.tar"
 echo "Creating $name…"
 docker images --filter "label=lingdb" -q | xargs docker save -o $name
-# Removing some to keep last 5:
-echo "Keeping only 5 latest backups…"
-ls -tr *tar | head -n -5 | xargs --no-run-if-empty rm
+# Removing some to keep last 2:
+echo "Keeping only 2 latest backups…"
+ls -tr *tar | head -n -2 | xargs --no-run-if-empty rm
