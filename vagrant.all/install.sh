a1dff2b96cffe41bebe8e8b88c710910a92729be
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2016-01-13 13:04:01 +0100

    Remove vagrant.*
    - Vagrant containers now have their own branch,
      and will likely not be developed further.
      Thus they are removed them from devel and master.

--- vagrant.all/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Updating package database:"
-apt-get update
-apt-get install -y docker.io pwgen
-cd /containerSetup
-./build.sh
-./run.sh
-# Installing upstart service:
-cp /containerSetup/vagrant.all/sndcomp.upstart /etc/init/sndcomp.conf
