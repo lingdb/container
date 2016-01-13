263753b1f0071fa0902696af1b3a97f68e680708
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2016-01-13 12:43:29 +0100

    Fix run.sh

--- run.sh
+++ run.sh
@@ -9,9 +9,11 @@ echo "Starting database containers…"
 echo "Sleeping 30s to wait for database containers to come up…"
 sleep 30s
 
-echo "Initializing database containers…"
-(cd mariadb/create && ./run.sh)
-(cd postgres/create && ./run.sh)
+# FIXME this is problematic iff run is to be used after restarts,
+# because it resets the database contents.
+#echo "Initializing database containers…"
+#(cd mariadb/create && ./run.sh)
+#(cd postgres/create && ./run.sh)
 
 echo "Starting backup containers…"
 (cd mariadb/backup && ./run.sh)
