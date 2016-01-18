3bdd28200ccd3a87e5f794f7879de2641fd41ddb
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2016-01-18 13:35:26 +0100

    Improve postgres/backup container
    - Dumps are now compressed with bzip2 instead of leaving them as raw .sql

--- postgres/backup/backup.sh
+++ postgres/backup/backup.sh
@@ -2,12 +2,16 @@
 # Name to use for the new database dump:
 cd /backup
 while true; do
+  t=$(date)
+  echo "Waking up, it's $t."
   name=$(date -I)
   echo "Creating backup $name…"
   pg_dump -h postgres -U ielexuser ielexdb201510 > $name.sql
+  echo "Compressing backup $name…"
+  bzip2 -f $name.sql
   # Keeping only 10 latest dumps:
   # Compare https://stackoverflow.com/a/10119963/448591
-  ls -tr *sql | grep -v 'create\|dump.sql' | head -n -10 | xargs --no-run-if-empty rm
+  ls -tr *sql.bz2 | grep -v 'create\|dump.sql' | head -n -10 | xargs --no-run-if-empty rm
   # Sleep until next backup:
   echo "Sleeping until next backup…"
   sleep 4h
