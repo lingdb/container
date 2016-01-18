2af04349e6d4e30becdbd81b7703548c5941cda9
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2016-01-18 21:05:41 +0100

    Add pull.sh
    - pull.sh is a dedicated script for only pulling the images.
      This is especially useful for updating purposes.
    - Modified build.sh to call pull.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-# Pulling all the base images that the containers build upon.
-images="continuumio/anaconda:latest ubuntu:14.04.3 mariadb:latest nginx:1.9.7 postgres:9.3.10 php:7.0.0-apache"
-echo "Pulling base images:"
-for image in $images; do
-  echo "Pulling image: $image"
-  docker pull $image
-done
 # https://stackoverflow.com/a/246128/448591
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $dir
+(./pull.sh)
 # Building containers…
 echo "Getting containers ready…"
 containers="mariadb/create mariadb/backup postgres/create postgres/backup sndcomp sndcomp.php ielex2 nginx nginx.sndcomp"
