05417c1fdf41d1c04748407857fb8d053e11284c
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2016-02-04 06:59:49 +0100

    Fix dependency for lingdb.upstart
    - Closes #18

--- run.sh
+++ run.sh
@@ -9,12 +9,6 @@ echo "Starting database containers…"
 echo "Sleeping 30s to wait for database containers to come up…"
 sleep 30s
 
-# FIXME this is problematic iff run is to be used after restarts,
-# because it resets the database contents.
-#echo "Initializing database containers…"
-#(cd mariadb/create && ./run.sh)
-#(cd postgres/create && ./run.sh)
-
 echo "Starting backup containers…"
 (cd mariadb/backup && ./run.sh)
 (cd postgres/backup && ./run.sh)
