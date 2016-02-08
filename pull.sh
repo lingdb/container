e6c085a8795fc719eb56b8e21854b3eeb916e184
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2016-02-08 16:24:19 +0100

    Update pull.sh

--- pull.sh
+++ pull.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Pulling all the base images that the containers build upon.
-images="continuumio/anaconda:latest ubuntu:14.04.3 mariadb:latest nginx:1.9.7 postgres:9.3.10 php:7.0.0-apache"
+images="debian:jessie ubuntu:14.04.3 mariadb:latest nginx:1.9.7 postgres:9.3.10 php:7.0.0-apache"
 echo "Pulling base images:"
 for image in $images; do
   echo "Pulling image: $image"
