56a30eb0b0458202b77df518a0f10b425a143426
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2016-02-23 15:37:19 +0100

    Adjust ielex2/stage1 container
    - Bump ielex2

--- ielex2/stage1/configIELex2.sh
+++ ielex2/stage1/configIELex2.sh
@@ -4,7 +4,11 @@ echo "Installing REQUIREMENTS"
 cd $dir
 pip install -r REQUIREMENTS
 #rm -rf .git{,ignore,modules}
+mv .git /tmp/git_configIELex2.sh
 cd static
+npm install
 bower install --allow-root ./bower.json
+grunt
 cd ..
+mv /tmp/git_configIELex2.sh .git
 bash
