513dfcbb3312a2b891a04657bd650fad8543cbea
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2016-06-17 13:13:38 +0200

    Add mapnik container
    - This container allows for local rendering of osm tiles.
      It works together with sndcomp.php/run.sh using the export parameter.
      This commit also depends on the leaflet.js branch of sndcomp.php,
      which shall get deployed soon.

---
+++ mapnik/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ ! -d "mapbox-studio-osm-bright.tm2" ]; then
+  git clone https://github.com/mapbox/mapbox-studio-osm-bright.tm2.git
+fi
+if [ ! -f "planet_z0-z5.mbtiles" ]; then
+  wget -c "https://osm2vectortiles-downloads.os.zhdk.cloud.switch.ch/v2.0/planet_z0-z5.mbtiles"
+fi
+docker run -v $(pwd):/data  \
+           -l lingdb=mapnik \
+           -d klokantech/tileserver-mapnik
--- sndcomp.php/run.sh
+++ sndcomp.php/run.sh
@@ -11,6 +11,22 @@ if [ -z "$last" ]; then
   echo "Not starting $image because lingdb_mariadb isn't running."
 else
   case $1 in
+    export)
+      echo "Running $image against $last to export…"
+      mapnik=$(docker ps -f "label=lingdb=mapnik" -f status=running -n=1 -q)
+      if [ -z "$mapnik" ]; then
+        echo "Not starting $image for export because mapnik isn't running."
+      else
+        docker run --link $last:mariadb \
+                   --link $mapnik:mapnik \
+                   --name $name \
+                   -v `pwd`/../sndcomp.php/src:/var/www/html \
+                   -v `pwd`/sound:/var/www/html/sound \
+                   -d $image
+        current=$(docker ps -f label=lingdb=sndcomp.php -f status=running -q)
+        docker exec -it $current /usr/local/bin/php -f /var/www/html/script/generateOffline.php
+      fi
+    ;;
     test)
       echo "Testing $image against $last…"
       docker run --link $last:mariadb \
