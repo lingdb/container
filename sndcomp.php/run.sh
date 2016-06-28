f61c1774121d700c99bd14508abb19ad15828cc0
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2016-06-28 15:56:33 +0200

    Bump sndcomp.php
    - Adjust run.sh to provide DEPLOYED env variable.
    - src is on 2016-06-28.0

--- sndcomp.php/run.sh
+++ sndcomp.php/run.sh
@@ -23,6 +23,7 @@ else
                    -v `pwd`/../sndcomp.php/src:/var/www/html \
                    -v `pwd`/sound:/var/www/html/sound \
                    -e EXPORT_TASK=$2 \
+                   -e DEPLOYED=true \
                    -d $image
         current=$(docker ps -f label=lingdb=sndcomp.php -f status=running -n=1 -q)
         docker exec -it $current /usr/local/bin/php -f /var/www/html/script/generateOffline.php
@@ -41,6 +42,7 @@ else
       docker run --link $last:mariadb \
                  --name $name \
                  -v `pwd`/sound:/var/www/html/sound \
+                 -e DEPLOYED=true \
                  -d $image
     ;;
   esac
