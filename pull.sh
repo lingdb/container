bc102556321740700589e76f2197ea3539b5901d
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2016-08-07 09:21:05 +0200

    Remove ubuntu:14.04.3 from pull.sh

--- pull.sh
+++ pull.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Pulling all the base images that the containers build upon.
-images="debian:jessie node:latest ubuntu:14.04.3 mariadb:latest nginx:1.9.7 postgres:9.3.10 php:7.0.0-apache"
+images="debian:jessie mariadb:latest nginx:1.9.7 node:latest php:7.0.0-apache postgres:9.3.10"
 echo "Pulling base images:"
 for image in $images; do
   echo "Pulling image: $image"
