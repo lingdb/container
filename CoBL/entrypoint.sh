4130bf15a84ba17acdd36f8859d81aaab06442cf
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2016-08-07 11:20:41 +0200

    Adjust CoBL/entrypoint.sh

--- CoBL/entrypoint.sh
+++ CoBL/entrypoint.sh
@@ -8,12 +8,7 @@ sed -i.bak "s/'USER': '',.*$/'USER':'ielexuser',/" $file
 sed -i.bak "s/'PASSWORD': '',.*$/'PASSWORD':'$POSTGRES_PASSWORD',/" $file
 sed -i.bak "s/'HOST': '',.*$/'HOST':'postgres',/" $file
 sed -i.bak "s/'PORT': '',.*$/'PORT': '5432',/" $file
-sed -i.bak "s/ALLOWED_HOSTS = \[/ALLOWED_HOSTS = \[\"127.0.0.1\"/" $file
-sed -i.bak "s/STATIC_ROOT = .\"/STATIC_ROOT = \"ielex\/static\/\"/" $file
 sed -i.bak "s/SECRET_KEY = \"<++>\"/SECRET_KEY = \"$DJANGO_SECRET\"/" $file
-sed -i.bak "s/project_long_name = u\"Test Lexical Cognacy Database\"/project_long_name = u\"CoBL\"/" $file
-sed -i.bak "s/project_short_name = u\"TestLex\"/project_short_name = u\"CoBL\"/" $file
-sed -i.bak "s/project_description = u\"\"\"Description for the front page\"\"\"/project_description = u\"\"\"Welcome to the CoBL website.\"\"\"/" $file
 rm $file.bak
 # Copy static files if requested:
 if [ -d /copyStatic ]; then
