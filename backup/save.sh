da11fb63ae5d8023575337adcbc99fe6616fbd45
Jakob Runge <mam09crm@studserv.uni-leipzig.de>
2016-09-15 08:44:59 +0200

    Improve xargs calls in backup/save.sh

--- backup/save.sh
+++ backup/save.sh
@@ -5,8 +5,8 @@ cd $dir
 # Removing current backups
 rm *.images.tar
 # Clenaing docker containers and images:
-docker ps -f status=exited -f label=lingdb -q | xargs docker rm
-docker images -f dangling=true -f label=lingdb -q | xargs docker rmi
+docker ps -f status=exited -f label=lingdb -q | xargs --no-run-if-empty docker rm
+docker images -f dangling=true -f label=lingdb -q | xargs --no-run-if-empty docker rmi
 # Creating new backup:
 name="$(date -I).images.tar"
 echo "Creating $name…"
