15839be1558ddabc558e047e863fc2e0837b3e03
Jakob Runge <sicarius@g4t3.de>
2016-11-24 22:26:38 +0100

    Improve CoBL container situation
    - This commit makes it possible for the CoBL containers
      to use environment variables for setting rather than manipulating
      the config files by using sed.
    
    - It also introduces the cobl_debug container that should aid Oleg
      in making it possible to run CoBL as a stand alone service
      with a clean/reduced database and debugging available.

--- CoBL/debug/entrypoint.sh
+++ CoBL/debug/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-# Copy static files:
-cp -rf /CoBL/static/* /copyStatic/
 # Migrate database:
+echo "migrate:"
 python manage.py migrate
+echo "exampleSetup:"
+python manage.py exampleSetup
+echo "runserver:"
 # Start gunicorn:
 pcount=$(grep -c "^processor" /proc/cpuinfo)
 wcount=$(python -c "print($pcount * 2 + 1)")
