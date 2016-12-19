50af0801d1d9a2ba29247ee1b2eadcc99f631321
Jakob Runge <sicarius@g4t3.de>
2016-12-19 17:55:48 +0100

    Adjust CoBL/debug to use python3

--- CoBL/debug/entrypoint.sh
+++ CoBL/debug/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Migrate database:
 echo "migrate:"
-python manage.py migrate
+python3 manage.py migrate
 echo "exampleSetup:"
-python manage.py exampleSetup
+python3 manage.py exampleSetup
 echo "runserver:"
 # Start gunicorn:
 pcount=$(grep -c "^processor" /proc/cpuinfo)
-wcount=$(python -c "print($pcount * 2 + 1)")
+wcount=$(python3 -c "print($pcount * 2 + 1)")
 gunicorn --workers $wcount --bind=$(hostname -i):5000 wsgi:application
