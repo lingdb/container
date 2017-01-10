477501125c6d1c74372484837231c084de59cf23
Jakob Runge <sicarius@g4t3.de>
2017-01-10 15:01:49 +0100

    Improve sndcomp.php setup
    - This commit comes in relation with the 12b41f0 commit in the soundcomparisons repository [1].
    
    [1]: https://github.com/lingdb/Sound-Comparisons/commit/12b41f0b0437fa91c31c48374ad57c1a1abd13e3

--- pull.sh
+++ pull.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Pulling all the base images that the containers build upon.
-images="debian:jessie mariadb:latest nginx:1.9.7 node:latest php:7.0.0-apache postgres:9.3.10"
+images="debian:jessie mariadb:latest nginx:1.9.7 node:latest php:7-apache postgres:9.3.10"
 echo "Pulling base images:"
 for image in $images; do
   echo "Pulling image: $image"
